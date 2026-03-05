2f0c397e80634f74f611ae573afc54302f8f4e82
Нателла Вардуль <natellavardul@MacBook-Air-Natella.local>
2026-03-05 22:22:32 +0200

    fix: tests using right module

--- tests/test_unit_core.py
+++ tests/test_unit_core.py
@@ -6,7 +6,7 @@ from quick_calc.core import add, sub, mul, div, to_decimal
 
 def test_add(): assert add(10, 3) == Decimal("13")
 
-def test_sub(): assert sub(15, 3) == Decimal("2")
+def test_sub(): assert sub(15, 3) == Decimal("12")
 
 def test_mul(): assert mul(10, 3) == Decimal("30")
 
@@ -17,7 +17,7 @@ def test_div_by_zero():
 
 def test_negative_numbers():
     assert add(-10, 3) == Decimal("-7")
-    assert sub(-7, -3) == Decimal("-10")
+    assert sub(-7, -3) == Decimal("-4")
 
 def test_decimal_numbers_from_strings():
     assert add("0.1", "0.2") == Decimal("0.3")
